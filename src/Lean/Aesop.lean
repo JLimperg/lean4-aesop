39ca645771367eba522b63d32709a502d3afd8a9
Jannis Limperg <jannis@limperg.de>
2021-06-28 20:09:07 +0200

    Minimise further

--- src/Lean/Aesop.lean
+++ src/Lean/Aesop.lean
@@ -1,16 +1,7 @@
-import Lean.ScopedEnvExtension
-
 -- Removing this line makes the error go away.
 import Lean.Meta
 
 open Lean
 
-builtin_initialize extension :
-    ScopedEnvExtension Name Name (List Name) ←
-  registerScopedEnvExtension {
-    name := `aesopExt
-    mkInitial := return []
-    ofOLeanEntry := λ rs r => return r
-    toOLeanEntry := λ r => return r
-    addEntry := λ rs r => r :: rs
-  }
+builtin_initialize
+  registerTraceClass `stuff
