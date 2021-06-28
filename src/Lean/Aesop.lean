f2ab02585801203940d33c44d0bd8e5971bcf686
Jannis Limperg <jannis@limperg.de>
2021-06-28 20:14:08 +0200

    Minimise further

--- src/Lean/Aesop.lean
+++ src/Lean/Aesop.lean
@@ -1,7 +1,9 @@
+import Lean.Util.Trace
+
 -- Removing this line makes the error go away.
 import Lean.Meta
 
-open Lean
+open Lean (registerTraceClass)
 
 builtin_initialize
   registerTraceClass `stuff
